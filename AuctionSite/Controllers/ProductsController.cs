441c0de365709f10cc7e4246dd736e4158b79410
Hui Shi <Hui Shi>
2016-09-11 17:19:17 +0200

    First commit

---
+++ AuctionSite/Controllers/ProductsController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace AuctionSite.Controllers
+{
+    public class ProductsController : Controller
+    {
+        // GET: Products
+        public ActionResult Details(int id)
+        {
+            ViewBag.id = id;
+            return View(); ;
+        }
+    }
+}
